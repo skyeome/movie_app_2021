dca85b55cd3a5b2afbac8537a97817bd2736d482
skyeome <tjru7230@naver.com>
2022-04-07 17:02:43 +0900

    Easy To Do List App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") return;
+    setToDos((current)=> [toDo,...current] );
+    setToDo("");
+  }
   return (
     <div>
-      <h1>Welcome!</h1>
+      <h2>My To Dos ({toDos.length})</h2>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="할 일을 입력 해주세요."/>
+        <button>할 일 추가</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((todo,idx)=><li key={idx}>{todo}</li>)}
+      </ul>
     </div>
   );
 }
