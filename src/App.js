42f514efdaf8faf9e9ba04e2e226a5c0dbb4e232
skyeome <tjru7230@naver.com>
2022-04-08 19:15:43 +0900

    Movie App - 00. add route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [current, setCurrent] = useState(0);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=50")
-      .then(response=> response.json())
-      .then(json=>{
-        setCoins(json);
-        setLoading(false);
-      });
-  },[]);
-  const onChange = (ev) => setCurrent(ev.target.value);
-  return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? "Loading..." : 
-      <div>
-        <input type="number" value={current} onChange={onChange} placeholder="How many dollars you get?" />
-        <br/>
-        <select>
-          {coins.map(coin=><option key={coin.id}>{coin.name} ({coin.symbol}): {current/(coin.quotes.USD.price)}</option>)}
-        </select>
-      </div>
-      }
-      
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="movie" element={<Detail />}></Route>
+    </Routes>
+  </Router>;
 }
 
 export default App;
